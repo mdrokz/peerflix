f0e8092a3015666d6f58da8f87f53bafc4296090
mdrokz <mohammadmunshi@gmail.com>
2022-05-23 00:31:25 +0530

    add index.d.ts for type support

---
+++ index.d.ts
@@ -0,0 +1,28 @@
+import { Server } from "http";
+
+interface TorrentOpts {
+    peerPort: number,
+    port: number,
+    host: string,
+    list: string[]
+}
+
+interface ServerOpts {
+    blocklist?: string,
+    index?: number,
+    type?: string,
+    filter?: <T>(v: T) => boolean;
+    sort?: <S>(a: S, b: S) => number;
+}
+
+type Magnet = string | Buffer;
+
+type PeerFlixOpts = ServerOpts & TorrentOpts;
+
+declare function parseBlocklist(filename: string): string[];
+
+declare function truthy(): boolean;
+
+declare function createServer(e: TorrentStream.TorrentEngine): Server;
+
+export default function (torrent: Magnet, opts: PeerFlixOpts);
